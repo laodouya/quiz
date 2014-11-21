c203952632cc2d4bda1e48a4383d3c11ec3fd7cd
laodouya <laodouya@yeah.net>
2014-11-21 13:54:36 +0800

    增加检查系统字节序大小端的程序

---
+++ big-or-little-endian.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    union ut{
+        short s;
+        char c[2];
+    }u;
+
+    if(sizeof(short) == 2)
+    {
+        u.s = 0x0102;
+        if(u.c[0] == 1 && u.c[1] == 2)
+        {
+            printf("big enidan/n");
+        }else if(u.c[0] == 2 && u.c[1] == 1)
+        {
+            printf("little endian.\n");
+        }
+        return 0;
+    }
+}
