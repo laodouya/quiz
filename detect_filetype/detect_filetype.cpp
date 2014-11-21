371800386c43eab1e2893ba7830b0fdb5db89e01
laodouya <laodouya@yeah.net>
2014-11-21 13:58:56 +0800

    增加检测magic库版本

--- detect_filetype/detect_filetype.cpp
+++ detect_filetype/detect_filetype.cpp
@@ -7,9 +7,11 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     const char * magicfile="/usr/share/file/magic";
+
     int flag = MAGIC_MIME_TYPE;
     magic_t magic = magic_open(flag);
     
+    cout<<magic_version()<<endl;
     magic_load(magic, magicfile);
     
     ifstream in(argv[1], ios::in|ios::binary);
@@ -19,7 +21,13 @@ int main(int argc, char* argv[])
     
     const char *ret = magic_buffer(magic, buff, len);
     
-    cout<<ret<<endl;
+    if (NULL == ret)
+    {
+        cout<<"ret is null"<<endl;
+        cout<<magic_error(magic)<<endl;
+    }
+    else
+        printf("%s\n",ret);
 
     return 0;
 }
